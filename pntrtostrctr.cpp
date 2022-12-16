db1cfe941337f5ed1e5f435930269393fa672ddb
rohitcodism <rohitpaula18i@gmail.com>
2022-12-16 21:21:26 +0530

    done

--- pntrtostrctr.cpp
+++ pntrtostrctr.cpp
@@ -1,24 +1,29 @@
-//#include<iostream>
-//using namespace std;
-//struct r{
-  //  int length;
-    //int breadth;
-//};
-//int main(){
-  //  struct r x={10,5};
-    //struct r *p=&x;
-    //(*p).length=20;
-    //(*p).breadth=15;
-//}
 #include<iostream>
 using namespace std;
-struct r{
+struct rect{
 int length;
 int breadth;
 };
 int main(){
-    struct r *p;
-    p=(struct r *)malloc(sizeof(struct r));
-    (*p).length=15;
-    (*p).breadth=20;
+//normal pointer
+  rect r={10,5};
+  cout<<r.length<<endl;
+  cout<<r.breadth<<endl;
+
+//pointer to strcuture 
+//if we use pointer the in "cout" we use "->" instead of "<<"
+ struct rect *p=&r;
+ p->length=15;
+ p->breadth=20;
+  cout<<p->length<<endl;
+  cout<<p->breadth<<endl;
+
+//pointer to structure with dynamic memory allocation
+  struct rect *x;
+  x= new rect;
+  p->length =20;
+  p->breadth = 35;
+  cout<<p->length<<endl;
+  cout<<p->breadth<<endl; 
+  return 0;
 }
