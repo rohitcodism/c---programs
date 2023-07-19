2a690d9de17dac1a3939a9d3da2f43c2335c09aa
rohitcodism <rohitpaula18i@gmail.com>
2023-07-19 23:13:20 +0530

    do it

--- ch_arr1.cpp
+++ ch_arr1.cpp
@@ -1,26 +1,23 @@
 #include<iostream>
 using namespace std;
 
-// void isMissing(int a[], int n){
-//     int sum = (n*(n+1))/2;
-//     int sum2, i;
-//     for(i = 0;i<n;i++){
-//         sum2  += a[i];
-//     }
-//     cout<<"The missing element is : "<<(sum2-sum)<<endl;
-// }
+void isMissing(int a[], int n){
+    int sum = (n*(n+1))/2;
+    int sum2, i;
+    for(i = 0;i<n;i++){
+        sum2  += a[i];
+    }
+    cout<<"The missing element is : "<<(sum2-sum)<<endl;
+}
 
 int main(){
-    int n, a[100], i;
-    cout<<"enter the length of the array : "<<endl;
+    int a[50], i, n;
+    cout<<"Enter the length of the array : "<<endl;
     cin>>n;
-    if(n<=100){
-        cout<<"enter the array elements : "<<endl;
-        for(i = 0;i<n;i++){
-            cin>>a[i];
-        }
+    for(i = 0;i<n;i++){
+        cin>>a[i];
     }
-    // isMissing(a, a[i-1]);
-    cout<<a[n-1]<<endl;
+    isMissing(a,n);
+    cout<<n;
     return 0;
 }
